6245220226917aa8dd2b4d810bbadba7a65e020e
euroR67 <mchamaev67@gmail.com>
2023-10-15 20:41:09 +0200

    ajout submenu css

---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+// ============================ Slide menu  ============================ //
+
+// Récupération des element du DOM pour le menu slide responsive
+const bars = document.querySelector('.fa-bars');
+const slideMenu = document.querySelector('.slide-menu');
+const close = document.querySelector('.fa-xmark');
+const overlay = document.querySelector('.overlay');
+
+// Ecouteur d'evenement pour ouvrir le menu
+bars.addEventListener('click', () => {
+    // On ajoute la classe active sur le slide menu
+    slideMenu.classList.add('slide-menu-active');
+    // On ajoute la classe active sur l'overlay
+    overlay.classList.add('active-overlay');
+    // On désactive le scroll du body
+    document.body.style.overflow = 'hidden';
+})
+
+// Ecouteur d'evenement pour fermer le menu
+close.addEventListener('click', () => {
+    // On retire la classe active du slide menu
+    slideMenu.classList.remove('slide-menu-active');
+    // On ajoute la classe active sur l'overlay
+    overlay.classList.remove('active-overlay');
+    // On désactive le scroll du body
+    document.body.style.overflow = 'auto';
+})
+
+// Ecouteur d'evenement pour fermer le menu si on clique sur l'overlay
+overlay.addEventListener('click', () => {
+    // On retire la classe active du slide menu
+    slideMenu.classList.remove('slide-menu-active');
+    // On ajoute la classe active sur l'overlay
+    overlay.classList.remove('active-overlay');
+    // On désactive le scroll du body
+    document.body.style.overflow = 'auto';
+})
+
+// Pour le submenu déroulant //
+const categorie = document.querySelector('.categorie');
+const subMenu = document.querySelector('.sub-menu');
+
+categorie.addEventListener('click', () => {
+    subMenu.classList.toggle('sub-menu-active')
+})
+
+// ================================================================= //
+
