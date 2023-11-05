8454624d251316450a9195f3ca824c5bdad152ef
euroR67 <mchamaev67@gmail.com>
2023-11-05 14:57:04 +0100

    gestion d'upload et delete de photo de profil

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CandidatController extends AbstractController
@@ -21,12 +22,48 @@ class CandidatController extends AbstractController
 
     // Méthode pour modifier le profil du candidat
     #[Route('/candidat/{id}/edit', name: 'app_candidat_edit')]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(CandidatType::class, $user);
         $form->handleRequest($request);
 
+        // Si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $deletePhoto = $form->get('deletePhoto')->getData();
+
+            $photo = $form->get('photo')->getData();
+
+            if($deletePhoto) {
+                // Suppression de l'ancienne photo du serveur
+                unlink($this->getParameter('photo_profil').'/'.$user->getPhoto());
+                // Suppression de la photo dans la BDD
+                $user->setPhoto(null);
+            }
+
+            // this condition is needed because the 'photo' field is not required
+            // so the file must be processed only when a file is uploaded
+            if($photo) {
+            $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
+            
+            try {
+                $photo->move(
+                    $this->getParameter('photo_profil'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ... handle exception if something happens during file upload
+            }
+
+            // updates the 'photoFilename' property to store the PDF file name
+            // instead of its contents
+            $user->setPhoto($newFilename);
+
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_candidat_edit', ['id' => $user->getId()]);
--- src/Form/CandidatType.php
+++ src/Form/CandidatType.php
@@ -7,9 +7,12 @@ use App\Entity\Ville;
 use App\Entity\Langue;
 use App\Entity\Metier;
 use App\Entity\Niveau;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -26,6 +29,26 @@ class CandidatType extends AbstractType
             ->add('photo', FileType::class, [
                 'required' => false,
                 'label' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Le format de l\'photo n\'est pas valide !',
+                    ])
+                ],
+            ])
+            ->add('deletePhoto', CheckboxType::class, [
+                'required' => false,
+                'label' => false,
+                'mapped' => false, 
+                'attr' => [
+                    'style' => 'display: none;', // Hide the field initially
+                ],
             ])
             ->add('cv', FileType::class, [
                 'required' => false,
@@ -81,6 +104,25 @@ class CandidatType extends AbstractType
                 'multiple' => false,
                 'label' => false,
             ])
+            // Add an event listener to conditionally display deletePhoto
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $user = $event->getData();
+                $form = $event->getForm();
+    
+                // Check if the user has a photo
+                $hasPhoto = $user && $user->getPhoto();
+    
+                // if hasPhoto is true , set deletePhoto attr style to display: block
+                // else set it to display: none
+                $form->add('deletePhoto', CheckboxType::class, [
+                    'required' => false,
+                    'label' => false,
+                    'mapped' => false, 
+                    'attr' => [
+                        'style' => $hasPhoto ? 'display: block;' : 'display: none;',
+                    ],
+                ]);
+            });
         ;
     }
 
