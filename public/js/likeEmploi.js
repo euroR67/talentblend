70e3e6cdeed7ff0e0653197a6a1734253feb982c
euroR67 <mchamaev67@gmail.com>
2024-01-16 12:02:12 +0100

    front onglet, chat

--- public/js/likeEmploi.js
+++ public/js/likeEmploi.js
@@ -42,3 +42,47 @@ document.querySelectorAll('.save-emploi-link').forEach(function(link) {
         });
     });
 });
+
+document.querySelectorAll('.action a').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // Vérifiez si l'emploi est toujours considéré comme un emploi sauvegardé
+        let row = link.closest('tr');
+        if (!row.classList.contains('saved')) {
+            return;
+        }
+
+        let url = link.href;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const MySwal = Swal.mixin({scrollbarPadding: false});
+            if (data.error) {
+                MySwal.fire({
+                    icon: 'error',
+                    title: data.error,
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+            } else {
+                MySwal.fire({
+                    icon: 'success',
+                    title: data.success,
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+
+                // Supprimer la ligne du tableau et la classe 'saved'
+                row.parentNode.removeChild(row);
+            }
+        });
+    });
+});
